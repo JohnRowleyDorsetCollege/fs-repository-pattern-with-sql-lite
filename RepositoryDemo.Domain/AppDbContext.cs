45cc96db74b48f2c996b940fcda31095c925a4d2
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-02-04 13:35:48 +0000

    Added db context

---
+++ RepositoryDemo.Domain/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryDemo.Domain
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<Student> Students { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+
+                options.Equals("Data Source=c:/sqlite/students2025-02-04.db");
+
+            }
+
+        }
+
+    }
+}
---
+++ RepositoryDemo.Domain/StudentRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryDemo.Domain
+{
+    public class StudentRepository
+    {
+        private readonly AppDbContext _context;
+        public StudentRepository(AppDbContext context)
+        {
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+
+        public void Add(string name, int age)
+        {
+            var student = new Student {
+                Name = name,
+                Age = age
+            };  
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            Console.WriteLine($"Added: {student.Name}, Age: {student.Age}");
+        }
+
+
+    }
+}
