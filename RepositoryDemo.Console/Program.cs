9179cc77c59c00deac4bccb7e31475af624d7af5
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-02-04 13:45:01 +0000

    Successful run 1

--- RepositoryDemo.Console/Program.cs
+++ RepositoryDemo.Console/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using RepositoryDemo.Domain;
 using System;
+using System.Xml.Linq;
 
 Console.WriteLine("Hello, World!");
 
@@ -10,4 +11,13 @@ SQLitePCL.Batteries_V2.Init();
 var options = new DbContextOptionsBuilder<AppDbContext>()
            .UseSqlite("Data Source=c:/sqlite/students2025-02-04.db")
 .Options;
+
+using var context = new AppDbContext(options);
+var repo = new StudentRepository(context);
+
+//var student1= new Student { Name = "John", Age = 22 };  
+//var student2 = new Student { Name = "Jane", Age = 23 };
+
+repo.Add( "John", 22);
+repo.Add("Jane", 23);
 
