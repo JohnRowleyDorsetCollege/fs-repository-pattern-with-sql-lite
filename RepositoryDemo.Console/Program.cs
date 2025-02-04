27d4d1dfc011b285050fa2fdb369e0fadd65290f
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-02-04 13:40:44 +0000

    Added sql lite packages

--- RepositoryDemo.Console/Program.cs
+++ RepositoryDemo.Console/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using RepositoryDemo.Domain;
+using System;
+
 Console.WriteLine("Hello, World!");
+
+SQLitePCL.Batteries_V2.Init();
+
+var options = new DbContextOptionsBuilder<AppDbContext>()
+           .UseSqlite("Data Source=c:/sqlite/students2025-02-04.db")
+.Options;
+
